b993305dce2509d3154306e1c1a481fa06e357e4
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2024-08-25 20:52:35 -0300

    Update

--- src/routes/_private/ticket-aberto-cliente.jsx
+++ src/routes/_private/ticket-aberto-cliente.jsx
@@ -9,9 +9,11 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle
 } from "@/components/ui/card";
+import { Input } from '@/components/ui/input';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import {
   Tabs,
@@ -19,8 +21,7 @@ import {
   TabsList,
   TabsTrigger
 } from "@/components/ui/tabs";
-import { Textarea } from '@/components/ui/textarea';
-import { SendHorizonal } from 'lucide-react';
+import { SendIcon } from 'lucide-react';
 
 export const Route = createFileRoute('/_private/ticket-aberto-cliente')({
   component: OpenTicket
@@ -47,41 +48,45 @@ function OpenTicket() {
           </div>
 
           <TabsContent value="main" className="grid grid-cols-[minmax(auto,65%)_minmax(auto,450px)] gap-2 items-start">
-            <div>
-              <ScrollArea className="h-[calc(100vh-215px)] flex flex-col pr-3">
-                {Array.from({ length: 5 }).map((_, i) => (
-                  <Card x-chunk="dashboard-01-chunk-0" key={`card_${i}`} className="mb-4">
-                    <CardHeader className="flex flex-row gap-4">
-                      <Avatar>
-                        <AvatarImage src="https://api.dicebear.com/9.x/personas/svg" alt="@person" className="bg-slate-100" />
-                        <AvatarFallback>UN</AvatarFallback>
-                      </Avatar>
+            <Card>
+              <CardContent className="p-4 space-y-4 overflow-y-auto h-[calc(100vh-11rem)] pr-0">
+                <ScrollArea className="h-full flex flex-col pr-4">
+                  {Array.from({ length: 5 }).map((_, i) => (
+                    <Card x-chunk="dashboard-01-chunk-0" key={`card_${i}`} className="mb-4 bg-muted">
+                      <CardHeader className="flex flex-row gap-2">
+                        <Avatar>
+                          <AvatarImage src="https://api.dicebear.com/9.x/personas/svg" alt="@person" className="bg-slate-50" />
+                          <AvatarFallback>UN</AvatarFallback>
+                        </Avatar>
 
-                      <div>
-                        <CardTitle>Usuário novo</CardTitle>
-                        <CardDescription>
-                          Empresa XPTO
-                        </CardDescription>
-                      </div>
-                    </CardHeader>
-                    <CardContent>
-                      <h2 className="text-2xl font-bold">Titulo do chamado</h2>
-                      <span className="text-gray-300 text-sm mb-8 flex">Criado em 05/03/2024</span>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam distinctio dolores libero sint hic, rem, eum dolor at consequuntur in laborum, perferendis quia vel odit tempora placeat expedita nihil perspiciatis!</p>
-                    </CardContent>
-                  </Card>
-                ))}
-              </ScrollArea>
+                        <div className="flex items-center justify-between w-full">
+                          <div>
+                            <CardTitle className="font-medium text-sm">Usuário novo</CardTitle>
+                            <CardDescription className="text-xs text-muted-foreground">
+                              5 dias atrás
+                            </CardDescription>
+                          </div>
+                          <p className="text-xs text-muted-foreground"></p>
+                        </div>
+                      </CardHeader>
+                      <CardContent>
+                        <p className="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam distinctio dolores libero sint hic, rem, eum dolor at consequuntur in laborum, perferendis quia vel odit tempora placeat expedita nihil perspiciatis!</p>
+                      </CardContent>
+                    </Card>
+                  ))}
+                </ScrollArea>
+              </CardContent>
 
-              <div className="relative">
-                <form className="relative" onSubmit={handleSubmitMessage}>
-                  <Textarea className="mt-6 min-h-24 resize-none bg-white" placeholder="Descreva aqui o que está acontecendo" name="description" />
-                  <Button type="submit" className="mt-4 absolute bottom-4 right-4" size="icon">
-                    <SendHorizonal className="w-4 h-4" />
+              <CardFooter className="border-t p-4">
+                <form className="flex items-center w-full space-x-2" onSubmit={handleSubmitMessage}>
+                  <Input id="message" placeholder="Descreva aqui o problema" className="flex-1" autoComplete="off" />
+                  <Button type="submit" size="icon">
+                    <SendIcon className="w-4 h-4" />
+                    <span className="sr-only">Send</span>
                   </Button>
                 </form>
-              </div>
-            </div>
+              </CardFooter>
+            </Card>
 
             <InputCardInfos className="sticky top-6 w-full" />
           </TabsContent>
