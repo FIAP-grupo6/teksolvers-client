428b379309980a6337ea67fbd0a9d80e96756bfb
Matheus De Nigris <matheusdenigris@hotmail.com>
2024-10-25 19:19:24 -0300

    update mock

--- src/mock/agents.js
+++ src/mock/agents.js
@@ -19,7 +19,7 @@ export default [
 
   {
     "id": 3,
-    "ativo": true,
+    "ativo": false,
     "nome": "R2-D2",
     "descricao": "Assistente responsável por definir o RESPONSÁVEL do chamado",
     "imagem": "https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Spooky&flip=true&backgroundColor=3949ab,d1d4f9&backgroundType=solid,gradientLinear&backgroundRotation=360,-360,-330,-320&eyes=sensor&mouth=diagram",
@@ -28,7 +28,7 @@ export default [
 
   {
     "id": 4,
-    "ativo": true,
+    "ativo": false,
     "nome": "Baymax",
     "descricao": "Assistente responsável por definir o TIPO do chamado",
     "imagem": "https://api.dicebear.com/9.x/bottts/svg?seed=Sammy",
@@ -37,7 +37,7 @@ export default [
 
   {
     "id": 5,
-    "ativo": true,
+    "ativo": false,
     "nome": "WALL-E",
     "descricao": "Assistente responsável por definir o NÍVEL do chamado",
     "imagem": "https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Harley",
@@ -55,7 +55,7 @@ export default [
 
   {
     "id": 7,
-    "ativo": true,
+    "ativo": false,
     "nome": "Ultron",
     "descricao": "Assistente responsável por documentar INSTRUÇÕES DE TRABALHO quando necessário",
     "imagem": "https://api.dicebear.com/9.x/bottts/svg?seed=Fluffy",
@@ -64,7 +64,7 @@ export default [
 
   {
     "id": 8,
-    "ativo": true,
+    "ativo": false,
     "nome": "Jarvis",
     "descricao": "Assistente responsável por coletar feedbacks e medir a SATISFAÇÃO do cliente",
     "imagem": "https://api.dicebear.com/9.x/bottts/svg?seed=Snickers",
